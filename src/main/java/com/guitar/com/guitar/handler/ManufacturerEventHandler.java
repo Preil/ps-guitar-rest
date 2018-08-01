f7af6c146c8b0488674fd5fc1b1b39c92591e939
preil <preil2000@gmail.com>
2018-08-01 16:32:18 +0300

    Validation check via EventHandler Component.

---
+++ src/main/java/com/guitar/com/guitar/handler/ManufacturerEventHandler.java
@@ -0,0 +1,21 @@
+package com.guitar.com.guitar.handler;
+
+import com.guitar.model.Manufacturer;
+import org.springframework.data.rest.core.annotation.HandleBeforeCreate;
+import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+@RepositoryEventHandler(Manufacturer.class)
+public class ManufacturerEventHandler {
+
+    @HandleBeforeCreate
+    public void handlerBeforeCreate(Manufacturer m) {
+        // do a check to see if new Manufacturer has valid fields values
+
+        // only allows to create new Manufacturers to be created in the active state
+        if(!m.getActive()){
+            throw new IllegalArgumentException("New Manufacturers must be 'active'");
+        }
+    }
+}
