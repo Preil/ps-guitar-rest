1502309f14a70567ac0367901533500c38e1d67b
preil <preil2000@gmail.com>
2018-08-01 16:37:42 +0300

    Authorization example via EvenHandler component.

--- src/main/java/com/guitar/com/guitar/handler/ManufacturerEventHandler.java
+++ src/main/java/com/guitar/com/guitar/handler/ManufacturerEventHandler.java
@@ -3,12 +3,14 @@ package com.guitar.com.guitar.handler;
 import com.guitar.model.Manufacturer;
 import org.springframework.data.rest.core.annotation.HandleBeforeCreate;
 import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Component;
 
 @Component
 @RepositoryEventHandler(Manufacturer.class)
 public class ManufacturerEventHandler {
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @HandleBeforeCreate
     public void handlerBeforeCreate(Manufacturer m) {
         // do a check to see if new Manufacturer has valid fields values
